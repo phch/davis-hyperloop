af3b19500eb55ea7685647601291328d46ab01a3
Trevor Glynn <tfglynn@ucdavis.edu>
2016-06-14 13:37:03 -0700

    Fix broken tests

---
+++ gui/tests/__init__.py
@@ -0,0 +1,4 @@
+from .test_hyperloop_app import *
+
+def main():
+    test_parse_args()
--- gui/tests/test_hyperloop_app.py
+++ gui/tests/test_hyperloop_app.py
@@ -3,8 +3,8 @@ import hyperloop_app as app
 
 def test_parse_args():
     test = ['localhost', '6666']
-    args = app.parse_args(args).__dict__
-    gold = {'ip': 'localhost', 'port': '6666', 'log': 2}
+    args = app.parse_args(test).__dict__
+    gold = {'ip': 'localhost', 'port': '6666', 'log': 2, 'source': 8880}
     assert args == gold
 
 if __name__ == '__main__':

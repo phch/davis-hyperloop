3428fa05bb39f15c98d692c5e8925fa2f712efa1
Trevor Glynn <tfglynn@ucdavis.edu>
2016-06-21 15:12:06 -0700

    Write code for sending and receiving

--- networking/Telemetry/Telemetry.ino
+++ networking/Telemetry/Telemetry.ino
@@ -1,19 +1,3 @@
-/*
- * This is a slightly modified version of UDPSendReceiveString.
-
- UDPSendReceiveString:
- This sketch receives UDP message strings, prints them to the serial port
- and sends an "acknowledge" string back to the sender
-
- A Processing sketch is included at the end of file that can be used to send
- and received messages for testing with a computer.
-
- created 21 Aug 2010
- by Michael Margolis
-
- This code is in the public domain.
- */
-
 #include <SPI.h> // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 #include <EthernetUdp.h> // UDP library from: bjoern@cs.stanford.edu 12/30/2008
@@ -21,63 +5,57 @@
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-IPAddress ip(192, 168, 2, 5);
 
-unsigned int localPort = 8888;
+// for receiving commands over TCP
+IPAddress local_host(192, 168, 2, 5);
+unsigned int local_port = 8001;
+EthernetServer tcp_server(local_port);
 
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
-char replyBuffer[] = "ACK\n";
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
+// for sending data over UDP
+IPAddress remote_host;
+unsigned int remote_port;
+EthernetUDP udp;
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial);
-  Serial.print("Establishing Network Connection...");
-  Ethernet.begin(mac, ip);
-  Serial.println("OK!");
-  Udp.begin(localPort);
+  while (!Serial)
+    ;
+  Ethernet.begin(mac, local_host);
+  
+  tcp_server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
-  // if there's data available, read a packet
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    printIPAddress(remote);
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
-    Serial.println("Contents:");
-    printPacket(packetSize);
-
-    // send a reply to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(replyBuffer);
-    Udp.endPacket();
-  }
-  delay(10);
+  recv_command();
+  send_data();
 }
 
-void printPacket(int packetSize) {
-  int readSize;
-  while (packetSize > 0) {
-    readSize = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    packetBuffer[readSize] = '\0';
-    Serial.print(packetBuffer);
-    packetSize -= readSize;
+void recv_command() {
+  static EthernetClient remote;
+
+  if (!remote.connected()) {
+    remote = tcp_server.available();
+    if (remote)
+      Serial.println("new client");
+    else
+      return;
   }
+
+  int bytes = remote.available();
+  if (bytes == 0)
+    return;
+
+  Serial.println("received data");
+  // TODO: deal with client messages
+  while (bytes-- > 0)
+    Serial.write(remote.read());
   Serial.println();
 }
 
-void printIPAddress(IPAddress ip) {
-  for (int i = 0; i < 3; i++) {
-    Serial.print(ip[i], DEC);
-    Serial.print('.');
-  }
-  Serial.print(ip[3], DEC);
+void send_data() {
+  static char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1] = "XXX"; // for testing
+  // TODO: get data from subsystems and actually send it
+  udp.write(packetBuffer);
 }

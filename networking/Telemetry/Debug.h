a190fc8cab4a13548904f3c49aee5d51edc81b09
Trevor Glynn <tfglynn@ucdavis.edu>
2016-06-24 15:28:47 -0700

    Add debugging macro

---
+++ networking/Telemetry/Debug.h
@@ -0,0 +1,10 @@
+#ifndef NDEBUG
+# define DEBUG_PRINT(msg) \
+do { \
+  Serial.print(__func__); \
+  Serial.print(": "); \
+  Serial.println(msg); \
+} while (0)
+#else
+# define DEBUG_PRINT(msg) ((void)0)
+#endif

12b318c81e4479bec47400b2c1bbc8fd849b0976
Trevor Glynn <tfglynn@ucdavis.edu>
2016-06-28 19:01:04 -0700

    Major additions to main UI

--- gui/ui/network_dialog.py
+++ gui/ui/network_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'network_dialog.ui'
 #
-# Created: Tue Jun 21 13:33:14 2016
+# Created: Tue Jun 28 14:01:36 2016
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
